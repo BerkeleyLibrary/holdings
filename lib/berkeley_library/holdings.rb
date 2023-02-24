0c7378193fd87ce20a867f762e704eaa62c3facf
David Moles <dmoles@berkeley.edu>
2023-02-24 14:00:24 -0800

    Initial setup with `bundle gem`

---
+++ lib/berkeley_library/holdings.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "holdings/version"
+
+module BerkeleyLibrary
+  module Holdings
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/berkeley_library/holdings_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe BerkeleyLibrary::Holdings do
+  it "has a version number" do
+    expect(BerkeleyLibrary::Holdings::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
