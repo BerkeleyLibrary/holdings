5ac848c2be8f45c80ede07f2bdc5338391145b07
David Moles <dmoles@berkeley.edu>
2023-03-06 16:33:14 -0800

    Add specs for finding OCLC numbers in spreadsheets

--- lib/berkeley_library/holdings/spreadsheets.rb
+++ lib/berkeley_library/holdings/spreadsheets.rb
@@ -1,3 +1,4 @@
+require 'marcel'
 require 'rubyXL'
 
 module BerkeleyLibrary
@@ -7,24 +8,67 @@ module BerkeleyLibrary
         include Spreadsheets
       end
 
-      OCLC_COL_HEADER = 'OCLC Number'
+      OCLC_COL_HEADER = 'OCLC Number'.freeze
 
-      def oclc_numbers_from(xlsx_path)
-        wb = RubyXL::Parser.parse(xlsx_path)
-        ws = wb.worksheets[0]
-        raise ArgumentError, 'Header row not found' unless (header_row = ws[0])
-        raise ArgumentError, "#{OCLC_COL_HEADER} column not found in #{row.size} columns" unless (oclc_col_index = find_oclc_col_index(header_row))
+      # .xlsx format, a.k.a. "Office Open XML Workbook" spreadsheet
+      MIME_TYPE_OOXML_WB = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'.freeze
 
-        ws.each_with_index do |row, ri|
-          next if ri == 0
-          next unless (cell = row[oclc_col_index])
-          next unless (v = cell.value)
-
-          yield v if block_given?
-        end
+      def oclc_numbers_from(xlsx_path, &)
+        wb = ensure_xlsx_workbook(xlsx_path)
+        enumerate_oclc_numbers(wb, &)
       end
 
       private
+
+      def ensure_xlsx_workbook(xlsx_path)
+        # RubyXL will try to parse an Excel 95 or 97 file (which are still)
+        # zip-based) but then choke when it tries to read the worksheet, so
+        # we explicitly check the MIME type here
+        check_mime_type!(xlsx_path)
+
+        RubyXL::Parser.parse(xlsx_path)
+      end
+
+      def check_mime_type!(xlsx_path)
+        xlsx_pathname = Pathname.new(xlsx_path)
+        mime_type = Marcel::MimeType.for(xlsx_pathname)
+        # TODO: test w/application/vnd.ms-excel.sheet.macroenabled.12
+        return if Marcel::Magic.child?(mime_type, MIME_TYPE_OOXML_WB)
+
+        raise ArgumentError, "Expected Excel Workbook (.xlsx), got #{mime_type}: #{xlsx_path}"
+      end
+
+      def enumerate_oclc_numbers(wb)
+        return to_enum(:enumerate_oclc_numbers, wb) unless block_given?
+
+        ws = wb.worksheets[0]
+        oclc_col_index = oclc_col_index_from(ws)
+
+        ws.each_with_index do |row, ri|
+          next unless ri > 0
+          next unless row
+          next unless (cell = row[oclc_col_index])
+          next unless (strval = strval_from(cell))
+
+          yield strval
+        end
+      end
+
+      def strval_from(cell)
+        return unless cell
+        return unless (v = cell.value)
+        return if (v_str = v.to_s).strip == ''
+
+        v_str
+      end
+
+      def oclc_col_index_from(ws)
+        raise ArgumentError, 'Header row not found' unless (header_row = ws[0])
+        raise ArgumentError, "#{OCLC_COL_HEADER} column not found in #{row.size} columns" unless
+          (oclc_col_index = find_oclc_col_index(header_row))
+
+        oclc_col_index
+      end
 
       def find_oclc_col_index(row)
         find_column_index(row) do |cell|
@@ -37,11 +81,7 @@ module BerkeleyLibrary
       end
 
       def find_column_index(row, &block)
-        (0...row.size).find do |ci|
-          next unless (cell = row[ci])
-
-          block.call(cell.value)
-        end
+        (0...row.size).find { |ci| block.call(row[ci]) }
       end
     end
   end
---
+++ spec/berkeley_library/holdings/spreadsheets_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+
+module BerkeleyLibrary
+  module Holdings
+    describe Spreadsheets do
+      describe :oclc_numbers_from do
+        let(:oclc_numbers_expected) { File.readlines('spec/data/excel/oclc_numbers_expected.txt', chomp: true) }
+
+        it 'returns an enumerator if no block given' do
+          xlsx_path = 'spec/data/excel/oclc-numbers.xlsx'
+          en = Spreadsheets.oclc_numbers_from(xlsx_path)
+          expect(en).to be_a(Enumerator)
+          expect(en.to_a).to eq(oclc_numbers_expected)
+        end
+
+        context 'success' do
+          it 'finds OCLC numbers as numbers' do
+            xlsx_path = 'spec/data/excel/oclc-numbers.xlsx'
+            expect { |b| Spreadsheets.oclc_numbers_from(xlsx_path, &b) }
+              .to yield_successive_args(*oclc_numbers_expected)
+          end
+
+          it 'finds OCLC numbers as strings' do
+            xlsx_path = 'spec/data/excel/oclc-numbers-text.xlsx'
+            expect { |b| Spreadsheets.oclc_numbers_from(xlsx_path, &b) }
+              .to yield_successive_args(*oclc_numbers_expected)
+          end
+
+          it 'skips blank cells' do
+            xlsx_path = 'spec/data/excel/oclc-numbers-sparse.xlsx'
+            expect { |b| Spreadsheets.oclc_numbers_from(xlsx_path, &b) }
+              .to yield_successive_args(*oclc_numbers_expected)
+          end
+
+          it 'finds OCLC numbers in later columns' do
+            xlsx_path = 'spec/data/excel/oclc-numbers-extra-cols.xlsx'
+            expect { |b| Spreadsheets.oclc_numbers_from(xlsx_path, &b) }
+              .to yield_successive_args(*oclc_numbers_expected)
+          end
+        end
+
+        context 'failure' do
+          it 'rejects an Excel 95 (.xls) workbook' do
+            xls_path = 'spec/data/excel/oclc-numbers-excel95.xls'
+
+            expect { Spreadsheets.oclc_numbers_from(xls_path).to_a }
+              .to raise_error(ArgumentError)
+          end
+
+          it 'rejects an Excel 97 (.xls) workbook' do
+            xls_path = 'spec/data/excel/oclc-numbers-excel97.xls'
+
+            expect { Spreadsheets.oclc_numbers_from(xls_path).to_a }
+              .to raise_error(ArgumentError)
+          end
+
+          it 'rejects an Excel 95 workbook renamed to .xlsx' do
+            xls_path = 'spec/data/excel/oclc-numbers-excel95-as-xlsx.xslx'
+
+            expect { Spreadsheets.oclc_numbers_from(xls_path).to_a }
+              .to raise_error(ArgumentError)
+          end
+        end
+      end
+    end
+  end
+end
